afb4e170801301cbab796b2128360243bb0a537e
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-08-28 22:13:54 +0300

    Add files via upload

---
+++ tasklist_project/tasks/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/create_task.html', {'form': form})
+
+def delete_task(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+    return redirect('task_list')
